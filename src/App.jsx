572c5b467409042651407e72b0a181db082f2e74
bd6981 <brittanyjdavis22@gmail.com>
2022-11-26 22:00:03 -0500

    structure created for react portfolio

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from './Comps/Header/Header.jsx'
+import Nav from './Comps/Nav/Nav.jsx'
+import About from './Comps/About/About.jsx'
+import Experience from './Comps/Experience/Experience.jsx'
+import Services from './Comps/Services/Services.jsx'
+import Portfolio from './Comps/Portfolio/Portfolio.jsx'
+import Contact from './Comps/Contact/Contact.jsx'
+import Footer from './Comps/Footer/Footer.jsx'
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Contact/>
+      <Footer/>
+
+    </>
+  )
+}
+
+export default App
