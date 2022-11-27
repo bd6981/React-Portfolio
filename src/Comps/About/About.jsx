46845d20abfb1333eafee8fe107e28f26d885fa7
bd6981 <brittanyjdavis22@gmail.com>
2022-11-27 01:59:30 -0500

    changed background image working on about Component

--- src/Comps/About/About.jsx
+++ src/Comps/About/About.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import "./About.css"
+import me from "../Header/me.png"
+import {FaAward} from 'react-icons/fa'
 
 const About = () => {
   return (
     <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={me} alt="me" />
+          </div>
+        </div>
+          <div className='about_content'></div>
+          <div className="about_cards">
+            <article className='about_card'>
+              <FaAward className='about_icon'/>
+              <h5>Experiance</h5>
+              <small>General Assembly</small>
+            </article>
+            <article className='about_card'>
+              <FaAward className='about_icon'/>
+              <h5>Experiance</h5>
+              <small>General Assembly</small>
+            </article>
+            <article className='about_card'>
+              <FaAward className='about_icon'/>
+              <h5>Experiance</h5>
+              <small>General Assembly</small>
+            </article>
+          </div>
+        
+      </div>
 
     </section>    
   )
--- src/Comps/Nav/Nav.jsx
+++ src/Comps/Nav/Nav.jsx
@@ -5,15 +5,17 @@ import {AiTwotoneHome} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {RiServiceFill} from 'react-icons/ri'
 import {TiMessages} from 'react-icons/ti'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#")
   return (
     <nav>
-      <a href="active"><AiTwotoneHome/></a>
-      <a href="#about"><BiUserPin/></a>
-      <a href="#experience"><BiBook/></a>
-      <a href="#services"><RiServiceFill/></a>
-      <a href="#contact"><TiMessages/></a>
+      <a href="#" onClick= {() => setActiveNav('#')} className={activeNav === "#" ? 'active' : ''}><AiTwotoneHome/></a>
+      <a href="#about" onClick= {() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUserPin/></a>
+      <a href="#experience" onClick= {() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
+      <a href="#services" onClick= {() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceFill/></a>
+      <a href="#contact" onClick= {() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><TiMessages/></a>
     </nav>
   )
 }
