035fe7b1a304f7977edd6e4279b0ad34361a722a
bd6981 <brittanyjdavis22@gmail.com>
2023-01-12 14:27:01 -0500

    updated cv

--- src/Comps/Header/CTA.jsx
+++ src/Comps/Header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import CV from './download.png'
+import Img from './CV.png'
 
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href={Img} download className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Lets Talk</a>
     </div>
   )
