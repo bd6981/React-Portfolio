f1c92ff843d1914d5c62f0027810d80ee77a3e51
bd6981 <brittanyjdavis22@gmail.com>
2023-09-26 00:00:41 -0400

    enlarged

--- src/Comps/Header/Header.jsx
+++ src/Comps/Header/Header.jsx
@@ -3,6 +3,7 @@ import "./Header.css"
 import CTA from './CTA'
 import me from "./IMG_2662.jpg"
 import HeaderSocial from './HeaderSocials'
+import img from "../../assets/fast-forward.png";
 
 const Header = () => {
   return (
@@ -10,18 +11,20 @@ const Header = () => {
       <div className="container header__container">
         <h2>Hello I'm</h2>
         <h1>Brittany Davis</h1>
-        <h3 className='text-light'>A Junior Fullstack Developer</h3>
-        <CTA/>
-        <HeaderSocial/>
+        <h3 className="text-light">A Junior Fullstack Developer</h3>
+        <CTA />
+        <HeaderSocial />
         {/* <div className="me">
           {/* <img src={me} alt="me"/> */}
 
         {/* </div>  */}
-        <a href="#contact" className='scroll__down' >Scroll Down</a>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+       
       </div>
     </header>
-  
-  )
+  );
 }
 
 export default Header
